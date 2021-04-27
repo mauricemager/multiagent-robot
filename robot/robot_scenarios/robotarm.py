99aca150fdb897c3d336de044524159e4fa189f9
maurice <mauricemager@gmail.com>
2021-04-27 12:17:36 +0200

    train robotarm scenario

--- robot/robot_scenarios/robotarm.py
+++ robot/robot_scenarios/robotarm.py
@@ -50,15 +50,18 @@ class Scenario(BaseScenario):
             agent.color = np.array([0.25,0.25,0.25])
             agent.state.lengths = world.arm_length * np.ones(world.num_joints)
             agent.state.angles = (2 * np.random.rand(world.num_joints) - 1) * math.pi
+            # agent.state.angles = (np.array([0.0,0.5])) * math.pi
             agent.state.p_pos = np.array(origins[i][:])
 
         # set properties for landmarks
         for i, object in enumerate(world.objects):
-            object.color = np.array([1, 0, 0])
+            object.color = np.array([0, 0, 1])
             object.state.p_pos = 0.2 * np.random.randn(world.dim_p) + np.array([0.5, 0.0])
 
         # set properties for goal
         world.goals[0].state.p_pos = - world.objects[0].state.p_pos
+        world.goals[0].color = np.array([1, 0, 0])
+
         # world.goals[0].state.p_vel = np.zeros(world.dim_p)
 
     def reward(self, agent, world):
@@ -70,23 +73,60 @@ class Scenario(BaseScenario):
 
     def observation(self, agent, world):
         # initialize observation variables
-        state_observations = (agent.state.angles / math.pi).tolist() + [agent.state.grasp]
-        object_pos = []
-        partners = []
-        # fill in object observation for every object in the environment
-        for object in world.objects:
-            # determine relative distance to every object in the environment
-            dist = np.sum(np.square(object.state.p_pos - agent.position_end_effector()))
-            object_pos += [dist]
-        # when partner agents available, gain their information
+        state_obs, object_obs, partner_obs, goal_obs = [], [], [], []
+        grasp_obs = [agent.state.grasp]
+        # update agent state observation
+        for i in range(world.num_joints + 1):
+            state_obs += agent.get_joint_pos(i).tolist()
+        # update object state observation
+        for i in range(len(world.objects)):
+            object_obs += world.objects[i].state.p_pos.tolist()
+        # update partner observation, if any (centralized observation, not relative)
         if len(world.agents) > 1:
             for partner in world.agents:
                 # only for partner agents
                 if agent.name != partner.name:
+                    # partner state observations
+                    for i in range(world.num_joints + 1):
+                        partner_obs += partner.get_joint_pos(i).tolist()
+                    # add partner grasp observation
+                    partner_obs += [partner.state.grasp]
+        # update goal observation
+        goal_obs = world.goals[0].state.p_pos.tolist()
+
+        return state_obs + grasp_obs + object_obs + partner_obs + goal_obs
+
+
+
+
+        # print(f' test partner obs = {partner_obs}')
+
                     # determine relative distance to other agent's end effector
-                    diff = partner.position_end_effector() - agent.position_end_effector()
-                    partners += [np.linalg.norm(diff)] + [partner.state.grasp]
+
+
+                    # diff = partner.position_end_effector() - agent.position_end_effector()
+                    # partner_obs += [np.linalg.norm(diff)] + [partner.state.grasp]
+
+        # print(f' test state_obs = {state_obs}')
+        # print(f" agent state test = {agent.state.angles}")
+        #
+        # state_observations = (agent.state.angles / math.pi).tolist() + [agent.state.grasp]
+        # object_pos = []
+        # partners = []
+        # # fill in object observation for every object in the environment
+        # for object in world.objects:
+        #     # determine relative distance to every object in the environment
+        #     dist = np.sum(np.square(object.state.p_pos - agent.position_end_effector()))
+        #     object_pos += [dist]
+        # # when partner agents available, gain their information
+        # if len(world.agents) > 1:
+        #     for partner in world.agents:
+        #         # only for partner agents
+        #         if agent.name != partner.name:
+        #             # determine relative distance to other agent's end effector
+        #             diff = partner.position_end_effector() - agent.position_end_effector()
+        #             partners += [np.linalg.norm(diff)] + [partner.state.grasp]
         # combine observations to a single numpy array
-        return state_observations + object_pos + partners
+        # return [0,0,0,0,0,0,0,0,0]
 
 
